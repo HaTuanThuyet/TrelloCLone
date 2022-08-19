3aa0df64cdbb85091b1a80acfdd86d8a94dd7e85
Thuyet <thuyetha25@gmail.com>
2022-08-19 13:40:01 +0700

    'initdata'

---
+++ src/components/Card/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Card.scss';
+
+export default function Card({ card }) {
+
+    return (
+        <li className='card-item'>
+            {card.cover && <img src={card.cover} className='card-cover' alt="thuyetha" />}
+            {card.title}
+        </li>
+    )
+}
